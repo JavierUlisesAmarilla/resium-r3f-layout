db41a180bb2bae225aa0388fcd56850d6a4d6d56
JavierUlisesAmarilla And <amarillajavierulises@gmail.com>
2024-04-04 21:46:22 -0300

    assetId

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ export const App = () => {
         className='relative flex flex-col w-full h-full'
         // assetId={2302071}
         // assetId={1521848}
-        // assetId={2482359}
-        assetId={2310587}
+        assetId={2482359}
+      // assetId={2310587}
       />
       {/* <Test/> */}
     </>
