9724e0ff4cbdddc2df45b1ee28ef7751460bf567
JavierUlisesAmarilla And <amarillajavierulises@gmail.com>
2024-04-10 19:00:37 -0300

    CustomLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,85 +1,75 @@
-import * as FlexLayout from 'flexlayout-react'
 import 'flexlayout-react/style/light.css'
 import {One} from './One'
-
-
-const model = FlexLayout.Model.fromJson({
-  global: {tabEnableClose: false},
-  borders: [
-    {
-      type: 'border',
-      location: 'bottom',
-      size: 100,
-      children: [
-        {
-          type: 'tab',
-          name: 'Four',
-          component: 'text',
-        },
-      ],
-    },
-    {
-      type: 'border',
-      location: 'left',
-      size: 100,
-      children: [],
-    },
-  ],
-  layout: {
-    type: 'row',
-    weight: 100,
-    children: [
-      {
-        type: 'tabset',
-        weight: 50,
-        selected: 0,
-        children: [
-          {
-            type: 'tab',
-            name: 'One',
-            component: 'one',
-          },
-        ],
-      },
-      {
-        type: 'tabset',
-        weight: 50,
-        selected: 0,
-        children: [
-          {
-            type: 'tab',
-            name: 'Two',
-            component: 'text',
-          },
-          {
-            type: 'tab',
-            name: 'Three',
-            component: 'text',
-          },
-        ],
-      },
-    ],
-  },
-})
+import {CustomLayout} from './Scene/Utils/CustomLayout'
 
 
 export const App = () => {
-  const factory = (node: FlexLayout.TabNode) => {
-    const component = node.getComponent()
-
-    if (component === 'one') {
-      return <One/>
-    }
-
-    if (component === 'button') {
-      return <button>{node.getName()}</button>
-    }
-  }
-
   return (
-    <FlexLayout.Layout
-      model={model}
-      factory={factory}
+    <CustomLayout
+      components={{
+        one: (<One/>),
+        two: (<div>Two</div>),
+        three: (<div>Three</div>),
+        four: (<div>Four</div>),
+      }}
+      json={{
+        global: {tabEnableClose: false},
+        borders: [
+          {
+            type: 'border',
+            location: 'bottom',
+            size: 100,
+            children: [
+              {
+                type: 'tab',
+                name: 'Four',
+                component: 'four',
+              },
+            ],
+          },
+          {
+            type: 'border',
+            location: 'left',
+            size: 100,
+            children: [],
+          },
+        ],
+        layout: {
+          type: 'row',
+          weight: 100,
+          children: [
+            {
+              type: 'tabset',
+              weight: 50,
+              selected: 0,
+              children: [
+                {
+                  type: 'tab',
+                  name: 'One',
+                  component: 'one',
+                },
+              ],
+            },
+            {
+              type: 'tabset',
+              weight: 50,
+              selected: 0,
+              children: [
+                {
+                  type: 'tab',
+                  name: 'Two',
+                  component: 'two',
+                },
+                {
+                  type: 'tab',
+                  name: 'Three',
+                  component: 'three',
+                },
+              ],
+            },
+          ],
+        },
+      }}
     />
   )
 }
