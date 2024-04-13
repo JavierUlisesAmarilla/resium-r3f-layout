3ef85318fb925417f8d609954bf854e803feb173
JavierUlisesAmarilla And <amarillajavierulises@gmail.com>
2024-04-12 21:26:48 -0300

    remove Billboard

--- src/Scene/ResiumScene/ResiumLabels.tsx
+++ src/Scene/ResiumScene/ResiumLabels.tsx
@@ -7,29 +7,18 @@ export const ResiumLabels = () => {
   const {tileset} = useZustand()
 
   return tileset && (
-    <>
-      <Resium.BillboardCollection modelMatrix={Cesium.Transforms.eastNorthUpToFixedFrame(tileset.boundingSphere.center)}>
-        <Resium.Billboard
-          position={Cesium.Cartesian3.ZERO}
-          image='billboard.jpg'
-          horizontalOrigin={Cesium.HorizontalOrigin.CENTER}
-          verticalOrigin={Cesium.VerticalOrigin.CENTER}
-          color={Cesium.Color.YELLOW}
-          sizeInMeters
-          width={80}
-          height={14}
-        />
-      </Resium.BillboardCollection>
-      <Resium.Entity position={tileset.boundingSphere.center}>
-        <Resium.LabelGraphics
-          text='LabelGraphics'
-          style={Cesium.LabelStyle.FILL}
-          horizontalOrigin={Cesium.HorizontalOrigin.CENTER}
-          verticalOrigin={Cesium.VerticalOrigin.CENTER}
-          fillColor={Cesium.Color.BLUE}
-          disableDepthTestDistance={Number.POSITIVE_INFINITY}
-        />
-      </Resium.Entity>
-    </>
+    <Resium.Entity position={tileset.boundingSphere.center}>
+      <Resium.LabelGraphics
+        text='LabelGraphics'
+        style={Cesium.LabelStyle.FILL}
+        showBackground
+        backgroundColor={Cesium.Color.YELLOW}
+        horizontalOrigin={Cesium.HorizontalOrigin.CENTER}
+        verticalOrigin={Cesium.VerticalOrigin.CENTER}
+        heightReference={Cesium.HeightReference.CLAMP_TO_TERRAIN}
+        fillColor={Cesium.Color.BLUE}
+        disableDepthTestDistance={Number.POSITIVE_INFINITY}
+      />
+    </Resium.Entity>
   )
 }
